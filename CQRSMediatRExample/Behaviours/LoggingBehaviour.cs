81d6a7520deb50a0a6090cbfbfddb7f05143ed90
rei <67372053+reiugit@users.noreply.github.com>
2024-07-15 22:50:44 +0200

    Changed Logging information.

--- CQRSMediatRExample/Behaviours/LoggingBehaviour.cs
+++ CQRSMediatRExample/Behaviours/LoggingBehaviour.cs
@@ -10,11 +10,9 @@ public class LoggingBehaviour<TRequest, TResponse>(ILogger<LoggingBehaviour<TReq
         RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
-        logger.LogInformation("  1.) Before handling '{Request}'", typeof(TRequest).Name);
-
         var response = await next();
 
-        logger.LogInformation("  2.) After having handled '{Request}'", typeof(TRequest).Name);
+        logger.LogInformation(">> '{Request}' was handled.", typeof(TRequest).Name);
 
         return response;
     }
