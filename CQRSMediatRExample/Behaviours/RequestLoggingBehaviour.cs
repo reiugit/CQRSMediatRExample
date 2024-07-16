4829bedbc83d682f157afe87ad7f69c16928e737
rei <67372053+reiugit@users.noreply.github.com>
2024-07-16 09:29:09 +0200

    removed where condition from behaviour

--- CQRSMediatRExample/Behaviours/RequestLoggingBehaviour.cs
+++ CQRSMediatRExample/Behaviours/RequestLoggingBehaviour.cs
@@ -5,7 +5,6 @@ namespace CQRSMediatRExample.Behaviours;
 public class RequestLoggingBehaviour<TRequest, TResponse>(ILogger<RequestLoggingBehaviour<TRequest, TResponse>> logger)
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
-    //where TResponse : Product
 {
     public async Task<TResponse> Handle(
         TRequest request,
