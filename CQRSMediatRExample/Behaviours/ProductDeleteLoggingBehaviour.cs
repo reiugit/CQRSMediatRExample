a6a04532d703f04bc4f16052f5b8edf8ffd6982b
rei <67372053+reiugit@users.noreply.github.com>
2024-07-16 11:02:21 +0200

    Added ProductDeleteLoggingBehaviour.

---
+++ CQRSMediatRExample/Behaviours/ProductDeleteLoggingBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using CQRSMediatRExample.Features.ProductFeatures.Commands.Handlers;
+
+namespace CQRSMediatRExample.Behaviours;
+
+public class ProductDeleteLoggingBehaviour<TRequest, TResponse>(ILogger<ProductDeleteLoggingBehaviour<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : DeleteProductCommand
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        logger.LogWarning(">> Product with ID '{ProductId}' is going to be deleted.\n", request.Id);
+
+        var response = await next();
+
+        return response;
+    }
+}
--- CQRSMediatRExample/Program.cs
+++ CQRSMediatRExample/Program.cs
@@ -4,6 +4,7 @@ using CQRSMediatRExample.Data;
 using CQRSMediatRExample.Behaviours;
 using CQRSMediatRExample.Features.ProductFeatures.Queries.Endpoints;
 using CQRSMediatRExample.Features.ProductFeatures.Commands.Endpoints;
+using CQRSMediatRExample.Features.ProductFeatures.Commands.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -12,6 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(RequestLoggingBehaviour<,>));
     builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(ProductLoggingBehaviour<,>));
     builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(ProductListLoggingBehaviour<,>));
+    builder.Services.AddSingleton<IPipelineBehavior<DeleteProductCommand,Unit>,
+                                  ProductDeleteLoggingBehaviour<DeleteProductCommand,Unit>>();
 }
 
 var app = builder.Build();
