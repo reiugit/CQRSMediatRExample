9eb659390b9d1e75cca605c3ac47f7dfbd4babff
rei <67372053+reiugit@users.noreply.github.com>
2024-07-16 11:03:04 +0200

    Removed curly braces

--- CQRSMediatRExample/Program.cs
+++ CQRSMediatRExample/Program.cs
@@ -7,15 +7,14 @@ using CQRSMediatRExample.Features.ProductFeatures.Commands.Endpoints;
 using CQRSMediatRExample.Features.ProductFeatures.Commands.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
-{
-    builder.Services.AddDbContext<AppDbContext>(o => o.UseInMemoryDatabase("AppDb"));
-    builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
-    builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(RequestLoggingBehaviour<,>));
-    builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(ProductLoggingBehaviour<,>));
-    builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(ProductListLoggingBehaviour<,>));
-    builder.Services.AddSingleton<IPipelineBehavior<DeleteProductCommand,Unit>,
-                                  ProductDeleteLoggingBehaviour<DeleteProductCommand,Unit>>();
-}
+
+builder.Services.AddDbContext<AppDbContext>(o => o.UseInMemoryDatabase("AppDb"));
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
+builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(RequestLoggingBehaviour<,>));
+builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(ProductLoggingBehaviour<,>));
+builder.Services.AddSingleton(typeof(IPipelineBehavior<,>), typeof(ProductListLoggingBehaviour<,>));
+builder.Services.AddSingleton<IPipelineBehavior<DeleteProductCommand, Unit>,
+                              ProductDeleteLoggingBehaviour<DeleteProductCommand, Unit>>();
 
 var app = builder.Build();
 {
